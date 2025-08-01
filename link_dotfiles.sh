3790b59bbfc3f69ae3f88ee62681527001cb683a
Morais <marcosviniciusm99@gmail.com>
2025-08-01 10:50:22 -0300

    feat/refactor: new shell file to link dots and folder organization

---
+++ link_dotfiles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DOTFILES=~/DotFiles
+
+echo "Criando links simbolicos dos dotfiles..."
+
+ln -sf $DOTFILES/Configs/.zshrc ~/.zshrc
+ln -sf $DOTFILES/Configs/.tmux.conf ~/.tmux.conf
+
+ln -sf $DOTFILES/Configs/.config/hypr ~/.config/hypr
+
+ln -sf $DOTFILES/Configs/.config/waybar/ ~/.config/waybar
+
+ln -sf $DOTFILES/Configs/.config/rofi/ ~/.config/rofi
+
+ln -sf $DOTFILES/Configs/.config/kitty/ ~/.config/kitty
+
+ln -sf $DOTFILES/Configs/.config/mako/ ~/.config/mako
+
+ln -sf $DOTFILES/Configs/.config/fastfetch/ ~/.config/fastfetch
+
+ln -sf $DOTFILES/Wallpapers/ ~/Imagens/Wallpapers
+
+echo "Concluido!"
