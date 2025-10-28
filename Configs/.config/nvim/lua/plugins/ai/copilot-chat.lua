428652eebcccfbe8225d76234f6340554b5541ad
Marcos Vinicius Morais <marcosviniciusm99@gmail.com>
2025-10-28 16:07:24 -0300

    fix: removed lazy-lock.json from nvim

--- Configs/.config/nvim/lua/plugins/ai/copilot-chat.lua
+++ Configs/.config/nvim/lua/plugins/ai/copilot-chat.lua
@@ -2,20 +2,38 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "main",
     cmd = "CopilotChat",
-    opts = function()
-        local user = vim.env.USER or "User"
-        user = user:sub(1, 1):upper() .. user:sub(2)
-        return {
-            auto_insert_mode = true,
-            question_header = "  " .. user .. " ",
-            assistant = "  Copilot ",
-            answer_header = "  Copilot ",
-            tool = "󰊳  Tool ",
-            window = {
-                width = 0.4,
-            },
-        }
-    end,
+    opts = {
+        model = 'claude-sonnet-4.5',
+        language = 'Portuguese',
+        temperature = 0.1,
+        window = {
+            layout = 'vertical',
+            width = 0.5,
+        },
+        auto_insert_mode = true,
+        headers = {
+            user = '👤 You',
+            assistant = '🤖 Copilot',
+            tool = '🔧 Tool',
+        },
+
+        separator = '━━',
+        auto_fold = true, -- Automatically folds non-assistant messages
+    },
+    -- opts = function()
+    --     local user = vim.env.USER or "User"
+    --     user = user:sub(1, 1):upper() .. user:sub(2)
+    --     return {
+    --         auto_insert_mode = true,
+    --         question_header = "  " .. user .. " ",
+    --         assistant = "  Copilot ",
+    --         answer_header = "  Copilot ",
+    --         tool = "󰊳  Tool ",
+    --         window = {
+    --             width = 0.4,
+    --         },
+    --     }
+    -- end,
     keys = {
         { "<c-s>", "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
         -- { "<leader>a", "", desc = "+AI", mode = { "n", "v" } },
